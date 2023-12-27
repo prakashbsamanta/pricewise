f3647ac2b131da0d1fc67384011270a5f176a58d
prakash.s <prakashbsamanta@gmail.com>
2023-12-27 18:53:21 +0530

    feat: add HeroCarousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import HeroCarousel from "@/components/HeroCarousel";
+import SearchBar from "@/components/SearchBar";
 import Image from "next/image";
 import React from "react";
 
 const Home = () => {
   return (
     <>
-      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+      <section className="px-6 md:px-20 py-24">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">
@@ -24,12 +26,19 @@ const Home = () => {
               Powerful, self serve product and growth analytics to help you
               convert, engage and retain more
             </p>
-            SearchBar
+            <SearchBar />
           </div>
-          HeroCarousel
+          <HeroCarousel />
         </div>
       </section>
-      <section className="trending-section"></section>
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {["apple iphone 1", "book", "sneakers"].map((product) => (
+            <div>{product}</div>
+          ))}
+        </div>
+      </section>
     </>
   );
 };
