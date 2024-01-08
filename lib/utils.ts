c5febf7e1b241321d8f5a22ad4b42f72ddb94e48
prakash.s <prakashbsamanta@gmail.com>
2024-01-08 22:52:05 +0530

    feat: GET method for cronjob

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,13 @@
 import { PriceHistoryItem, Product } from "@/types";
 
+const Notification = {
+  WELCOME: "WELCOME",
+  CHANGE_OF_STOCK: "CHANGE_OF_STOCK",
+  LOWEST_PRICE: "LOWEST_PRICE",
+  THRESHOLD_MET: "THRESHOLD_MET",
+};
+const THRESHOLD_PERCENTAGE = 40;
+
 export function extractPrice(...elements: any) {
   for (const element of elements) {
     const priceText = element.text().trim();
@@ -70,4 +78,23 @@ export function getAveragePrice(priceList: PriceHistoryItem[]) {
   const averagePrice = sumOfPrices / priceList.length || 0;
 
   return averagePrice;
+}
+
+export function getEmailNotifType(
+  scrapedProduct: Product,
+  currentProduct: Product
+) {
+  const lowestPrice = getLowestPrice(currentProduct.priceHistory);
+
+  if (scrapedProduct.currentPrice < lowestPrice) {
+    return Notification.LOWEST_PRICE as keyof typeof Notification;
+  }
+  if (!scrapedProduct.isOutOfStock && currentProduct.isOutOfStock) {
+    return Notification.CHANGE_OF_STOCK as keyof typeof Notification;
+  }
+  if (scrapedProduct.discountRate >= THRESHOLD_PERCENTAGE) {
+    return Notification.THRESHOLD_MET as keyof typeof Notification;
+  }
+
+  return null;
 }
