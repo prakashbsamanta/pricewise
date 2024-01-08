bdd10714b9db8f3f1d250c66e60d153d0c2e4461
prakash.s <prakashbsamanta@gmail.com>
2024-01-08 23:13:22 +0530

    fix: maxDuration fixed

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -10,7 +10,7 @@ import {
   getLowestPrice,
 } from "@/lib/utils";
 
-export const maxDuration = 300;
+export const maxDuration = 10;
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 export async function GET() {
